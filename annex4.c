a4d49ba1986b30cc6e1c24078a52972c1b5b001e
AD <antoine.delorme@me.com>
2019-12-05 16:41:56 +0100

    bug corrected

--- annex4.c
+++ annex4.c
@@ -19,17 +19,17 @@ void clean_tableau(char *tab, int size, char *letters)
 		ft_memcpy(tab + i * CHAR, letters , CHAR);
 }
 
-char *gen_tableau(t_liste solution, int size)
+int gen_tableau(t_liste solution, int size, char **tab)
 {
 	int i;
 	int j;
 	int k;
-	char *tableau;
 	char *letters;
 	
-	tableau = (char*)ft_memalloc(size * size * sizeof(char) * CHAR);
+	if(!(*tab = (char*)ft_memalloc(size * size * sizeof(char) * CHAR)))
+		return (-1);
 	letters = LETTERS;	
-	clean_tableau(tableau, size, letters);
+	clean_tableau(*tab, size, letters);
 	i = -1;
 	while (++i < solution.nb)
 	{
@@ -38,34 +38,31 @@ char *gen_tableau(t_liste solution, int size)
 		{
 			j = -1;
 			while (++j < solution.pieces[i].width)
-				ft_memmove(tableau + CHAR * ((solution.pieces[i].pos_sol/size + k)*size + solution.pieces[i].pos_sol%size + j), solution.pieces[i].data_compressed[j + k* solution.pieces[i].width] ? letters + (i + 1) * CHAR : tableau + CHAR * ((solution.pieces[i].pos_sol/size + k)*size + solution.pieces[i].pos_sol%size + j), CHAR);
+				ft_memmove(*tab + CHAR * ((solution.pieces[i].pos_sol/size + k)*size + solution.pieces[i].pos_sol%size + j), solution.pieces[i].data_compressed[j + k* solution.pieces[i].width] ? letters + (i + 1) * CHAR : *tab + CHAR * ((solution.pieces[i].pos_sol/size + k)*size + solution.pieces[i].pos_sol%size + j), CHAR);
 		}
 	}
-	return (tableau);
+	return (1);
 }
 
-void affiche_solution(t_liste solution, int size)
+int affiche_solution(t_liste solution, int size)
 {
-	char i;
 	int j;
 	int k;
 	char *tableau;
 	
-	tableau = gen_tableau(solution, size);
-	i = 0;
+	tableau = NULL;
+	if(!gen_tableau(solution, size, &tableau))
+		return (-1);
 		k = 0;
 		while (k < size)
 		{
 			j = 0;
 			while (j < size)
-			{
-				write(1, tableau + (j + k *size) * CHAR, CHAR);
-				j++;
-			}
+				write(1, tableau + ((j++) + k *size) * CHAR, CHAR);
 			ft_putchar('\n');
 			k++;
 		}
-		i++;
 	ft_strdel(&tableau);
+	return (1);
 }
 
